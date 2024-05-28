ff04b516a41c86b1fc425901c5b8959bbb20cc87
LutherLin <1265041265@qq.com>
2024-05-28 22:36:15 +0800

    1

--- DataCollection/config.py
+++ DataCollection/config.py
@@ -4,7 +4,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='quantum_t')
     parser.add_argument('--save_path', default='./')
     parser.add_argument('--date', type=str, default="20240517", help="Start date in YYYYMMDD format")
-    parser.add_argument('--bar_size', default='1 hour')
+    parser.add_argument('--bar_size', default='1 min')
     parser.add_argument('--num_days', default = 10 ,type=int,help="number of days to download")
     # parser.add_argument('--duration', default='30 Y')lastTradeDateOrContractMonth
     parser.add_argument('--contract_symbol', default='QQQ',type=str,help="contract symbol,")
