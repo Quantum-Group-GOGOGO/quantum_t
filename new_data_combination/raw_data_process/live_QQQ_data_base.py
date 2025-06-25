b8b7f19ff0599792cac0861243f698b9244c5a0b
Wentian <littlenova223@gmail.com>
2025-06-25 10:43:26 -0400

    2025 June 25th v1

--- new_data_combination/raw_data_process/live_QQQ_data_base.py
+++ new_data_combination/raw_data_process/live_QQQ_data_base.py
@@ -1,4 +1,5 @@
 from ib_insync import *
+from env import *
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,12 +9,12 @@ import pandas as pd
 from tqdm import tqdm
 
 ib = IB()
-ib.connect('127.0.0.1', 7597, clientId=1)
+ib.connect('127.0.0.1', 4002, clientId=1)
 ib.reqMarketDataType(1)
 
 def request_many_day_QQQ(daysN):
     now=datetime.now()
-    dfs = pd.DataFrame(columns=['date','open','high','low','close','volume'])
+    dfs = pd.DataFrame(columns=['datetime','open','high','low','close','volume'])
     dfs.set_index('date', inplace=True)
     for day in tqdm(range(daysN), desc='Processing days'):
         endtime= now - timedelta(days=day)
@@ -27,8 +28,11 @@ def request_many_day_QQQ(daysN):
             useRTH=False,                  # 包括盘前盘后（如只要正常交易时段，设为 True）
             formatDate=1                   # 返回的 date 字段为 Python datetime
         )
-        df = util.df(bars)[['date', 'open', 'high', 'low', 'close', 'volume']].set_index('date')
-        dfs = pd.concat([df, dfs])
+        df = util.df(bars)[['datetime', 'open', 'high', 'low', 'close', 'volume']].set_index('date')
+        if dfs is None:
+            dfs = df
+        else:
+            dfs = pd.concat([df, dfs])
     dfs = dfs[~dfs.index.duplicated(keep='last')]
     return dfs
 
@@ -43,8 +47,8 @@ def request_1_day_QQQ():
         useRTH=False,                  # 包括盘前盘后（如只要正常交易时段，设为 True）
         formatDate=1                   # 返回的 date 字段为 Python datetime
     )
-    df = util.df(bars)[['date', 'open', 'high', 'low', 'close', 'volume']]
-    df.set_index('date', inplace=True)
+    df = util.df(bars)[['datetime', 'open', 'high', 'low', 'close', 'volume']]
+    df.set_index('datetime', inplace=True)
     return df
 
 def request_10_min_QQQ():
@@ -58,13 +62,14 @@ def request_10_min_QQQ():
         useRTH=False,                  # 包括盘前盘后（如只要正常交易时段，设为 True）
         formatDate=1                   # 返回的 date 字段为 Python datetime
     )
-    df = util.df(bars)[['date', 'open', 'high', 'low', 'close', 'volume']]
-    df.set_index('date', inplace=True)
+    df = util.df(bars)[['datetime', 'open', 'high', 'low', 'close', 'volume']]
+    df.set_index('datetime', inplace=True)
     return df
 
 def sync_QQQ_base():
     QQQ_type0_path=live_data_base+'/type0/QQQ/QQQ_BASE.pkl'
     QQQBASE= pd.read_pickle(QQQ_type0_path)
+    print(len(QQQBASE))
     last_BASE_time=QQQBASE.index[-1]
     now = datetime.now()
     delta = now - last_BASE_time    # 这是一个 timedelta 对象
@@ -79,8 +84,12 @@ def sync_QQQ_base():
     # 3. 切片：只保留 QQQBASE 中索引 < first_new_idx 的那部分
     base_trimmed = QQQBASE.iloc[:pos]
     # 4. 直接上下拼接
+    print(base_trimmed.head())
+    print(df.head())
     merged = pd.concat([base_trimmed, df])
     #merged重新赋值回QQQBASE
+    merged.to_pickle(live_data_base+'/type0/QQQ/QQQ_BASE1.pkl')
+    print(len(merged))
 
 def main():
     # 1. 连接 IB Gateway / TWS
@@ -98,6 +107,7 @@ def main():
     #df.set_index('date', inplace=True)
     #df=request_1_day_QQQ()
     df=request_10_min_QQQ()
+    sync_QQQ_base()
     # 6. 打印或返回
     print(df.head())
     print(df.tail())
